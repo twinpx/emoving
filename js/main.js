a2c4120765012b4832a7e5d9d6e8503412b01c94
Alexey PSD to HTML <psdtohtml.ninja@gmail.com>
2015-07-21 15:23:29 +0300

    New_form

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ $(function() {
   if ($('select').length) {
     $('select').styler();
   };
+
+  $('.fileup').click(function(){
+    $('.fileup-hidden').trigger('click');
+  });
+
+  $('.fileup-hidden').on('change', function (event, files, label) {
+    var file_name = this.value.replace(/\\/g, '/').replace(/.*\//, '')
+    $('.fileup').val(file_name);
+  });
+
 });
