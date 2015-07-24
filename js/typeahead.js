34a9ed2e2177c778a12b889e0343ef8500a8ee60
Alexey PSD to HTML <psdtohtml.ninja@gmail.com>
2015-07-24 10:34:17 +0300

    Ad_form_js

---
+++ js/typeahead.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function() {
+    var $input = $('.typeahead');
+	var cities = [];
+
+	$.getJSON( "json/jsonCities.js", {
+      	name: "name",
+    	outbound: "outbound",
+    	inbound: "inbound"
+    })
+    .done(function( data ){
+        	$.each( data, function( i, item ){
+        		cities.push( item );
+        	});
+
+        })
+    .complete(function(){
+        $input
+        	.removeAttr('disabled')
+        	.typeahead({
+                source: cities.map(function(item) {
+                    return item.name;
+                }),
+                autoSelect: false
+            });
+    });
+});
