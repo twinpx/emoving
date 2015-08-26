e64945013c3d85cef51379614e89388ae0c55c15
Alexey PSD to HTML <psdtohtml.ninja@gmail.com>
2015-08-26 20:38:55 +0300

    Fix_js

--- js/main.js
+++ js/main.js
@@ -33,6 +33,13 @@ $(function() {
       });
   }
 
-
+  if($('.js_only_number').length){
+    $('.js_only_number').on('keypress', function(evt){
+      var charCode = (evt.which) ? evt.which : event.keyCode
+      if (charCode > 31 && (charCode < 48 || charCode > 57))
+          return false;
+      return true;
+    })
+  }
 
 });
